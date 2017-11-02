71156d06ead0fe306c47b63ac0e726097072a514
Riley Hillstrom <Riley.Hillstrom@gmail.com>
2017-11-02 14:57:21 -0500

    Add files via upload

---
+++ Launch Sim/LaunchSimMain.m
@@ -0,0 +1,17 @@
+%%LPRD Launch Simulator
+% Riley Hillstrom and Joey Prom, 2017
+% Designed for both AEM 3101 class assignment and for future LPRD rocket
+% design
+%
+% Find on Github: https://github.com/RileyHillstrom/LPRD-Launch-Sim.
+
+
+%% Initial Design Parameters
+
+
+
+
+
+
+
+%% (Calculation #1)
