d601ecf857ff5edcf53282f7b468bde006acd6d9
RileyHillstrom <Riley.Hillstrom@gmail.com>
2017-11-22 11:12:50 -0600

    Test

--- Launch Sim/LaunchSimMain.m
+++ Launch Sim/LaunchSimMain.m
@@ -1,10 +1,11 @@
 %%LPRD Launch Simulator
 % Joey Prom and Riley Hillstrom, 2017
 % Designed for AEM 3101 class assignment and for future LPRD rocket
-% design
+% designe
 %
 % Find on Github: https://github.com/RileyHillstrom/LPRD-Launch-Sim.
 
+%test
 
 %% Initial Design Parameters
 
